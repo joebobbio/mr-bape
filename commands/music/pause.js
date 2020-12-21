756dec0546425f47257ab8e7a82f06608b537b1e
phene <60532517+joebobbio@users.noreply.github.com>
2020-12-21 18:50:33 -0500

    Add files via upload

---
+++ commands/music/pause.js
@@ -0,0 +1,21 @@
+module.exports = {
+	name: 'pause',
+	description: 'pause a song that\'s playing',
+	cooldown: 2,
+	cd: "No need to double check if its paused",
+	execute(message, args, d) {
+		const serverQueue = message.client.queue.get(message.guild.id);
+		if (serverQueue && serverQueue.playing) {
+			serverQueue.playing = false;
+			serverQueue.connection.dispatcher.pause();
+			const p = new d.Discord.MessageEmbed()
+				.setColor('#dd2de0')
+				.setTitle('Song')
+				.addField(`Paused.`, '_')
+				.setTimestamp()
+				.setFooter('DJ Grape');
+			return message.channel.send(p);
+		}
+		return message.channel.send("Can't if there\'s no music bruh");
+	}
+};
