d42a4be38413c88ec2a3599001e36e262deb9e0e
phene <60532517+joebobbio@users.noreply.github.com>
2020-12-22 00:09:18 -0500

    My code is the best code

--- commands/economy/craft.js
+++ commands/economy/craft.js
@@ -1,20 +1,20 @@
 const recipe = require('../../utils/recipes');
 module.exports = {
     name: "craft",
-    description: 'craft items using ores!',
+    description: 'Craft items using ores!',
     cooldown: 2,
-    cd: 'You can craft later bruh',
+    cd: `carpal tunnel syndrome: activate`,
     fan: true,
     async execute(message, args, d) {
         let argument = args.join(' ').toLowerCase();
         const numberMatch = /\d+/g;
         let inv = await d.items.get(message.author.id);
-        if (!inv || !inv.ore) {return message.channel.send('You don\'t have any ores!')}
+        if (!inv || !inv.ore) {return message.channel.send(`You don't have any ores!`)}
         if (!argument) { return message.channel.send(`Do ${d.prefix}recipe to see what you can craft!`) }
         if (Object.keys(recipe).some(e => argument.includes(e))) {
             let craft = Object.keys(recipe).find(e => argument.includes(e));
             let numItems = parseInt(argument.match(numberMatch));
-            if (numItems === 0) { return message.channel.send('ur not funny kid'); }
+            if (numItems === 0) { return message.channel.send('You have to craft at least SOMETHING'); }
             if (isNaN(numItems) || numItems < 1) { numItems = 1; }
             for (const key in recipe[craft]) {
                 if (typeof recipe[craft][key] === 'string') { continue; }
@@ -24,7 +24,7 @@ module.exports = {
                         .setTitle(message.author.username + `'s craftin`)
                         .addField('Failed', `You don\'t have all the things you need to make ${craft}(s)!`)
                         .setTimestamp()
-                        .setFooter('The Grape Blacksmith');
+                        .setFooter('The Bape Blacksmith');
                     return message.channel.send(e);
                 }
                 inv.ore[key] -= recipe[craft][key] * numItems;
@@ -37,7 +37,7 @@ module.exports = {
                 .setTitle(message.author.username + `'s craftin`)
                 .addField('Success!', `You made ${numItems} ${craft}(s)!`)
                 .setTimestamp()
-                .setFooter('The Grape Blacksmith');
+                .setFooter('The Bape Blacksmith');
             return message.channel.send(done);
         }
         else {
@@ -46,7 +46,7 @@ module.exports = {
                 .setTitle(message.author.username + `'s craftin`)
                 .addField('Failed', 'That item doesn\'t exist!')
                 .setTimestamp()
-                .setFooter('The Grape Blacksmith');
+                .setFooter('The Bape Blacksmith');
             return message.channel.send(e);
         }
     }
