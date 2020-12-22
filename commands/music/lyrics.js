a8f0cc4a0b3ca0c8f9bbf4c63f9acf259635a760
phene <60532517+joebobbio@users.noreply.github.com>
2020-12-22 17:14:59 -0500

    My code is the best code

--- commands/music/lyrics.js
+++ commands/music/lyrics.js
@@ -2,13 +2,13 @@ const { KSoftClient } = require('@ksoft/api');
 const ksoft = new KSoftClient(process.env.KSOFT);
 module.exports = {
     name: 'lyrics',
-    description: 'get lyrics of a song',
+    description: 'Gets the lyrics of a song.',
     cooldown: 1,
     aliases: ['lyr'],
-    cd: "Chill on the karaoke kid",
+    cd: "how can you read an entire song in less than a second",
     async execute(message, args, d) {
         const q = message.client.queue.get(message.guild.id);
-        if (!args.length && !q) { return message.channel.send('Give me something to search up bruh') }
+        if (!args.length && !q) { return message.channel.send('Please enter a song to get lyrics.') }
         let argument = args.join(' ')
         if (!argument) { argument = q.songs[0].title; }
         let { name, lyrics, url, artwork } = await ksoft.lyrics.get(argument);
@@ -16,7 +16,7 @@ module.exports = {
             .setColor('#dd2ed0')
             .setTitle(name.charAt(0).toUpperCase() + name.slice(1))
             .setThumbnail(artwork)
-            .setFooter('DJ Grape | Provided by KSoft.Si')
+            .setFooter('DJ Bape | Provided by KSoft.Si')
         if (name.length + lyrics.length > 6000) { lyricEmbed.addField('The lyrics are too long, here is the URL!', url); }
         else if (lyrics.length > 1024) {
             let arr = lyrics.split('\n\n');
