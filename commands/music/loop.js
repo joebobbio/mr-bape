2e27b25b0537f316790b413e69ffff0534f68b68
phene <60532517+joebobbio@users.noreply.github.com>
2020-12-22 17:11:59 -0500

    My code is the best code

--- commands/music/loop.js
+++ commands/music/loop.js
@@ -1,18 +1,18 @@
 module.exports = {
     name: 'loop',
-    description: 'get see what song is currently playin',
+    description: 'Loop the current song or queue.',
     cooldown: 1,
     aliases: ['repeat'],
-    cd: "Don't go loopy",
+    cd: "The music may be forever, but you aren't.",
     execute(message, args, d) {
         const serverQueue = message.client.queue.get(message.guild.id);;
-        if (!serverQueue) return message.channel.send('There\'s nothin playin!');
+        if (!serverQueue) return message.channel.send(`No music is playing!`);
         if (serverQueue.repeatMode === 0) {
             serverQueue.repeatMode = 1;
-            message.channel.send('Now looping through the **song**')
+            message.channel.send('Now looping the current song.')
         } else if (serverQueue.repeatMode === 1) {
             serverQueue.repeatMode = 2;
-            message.channel.send('Now looping through the **queue**')
+            message.channel.send('Now looping the current queue')
         } else if (serverQueue.repeatMode === 2) {
             serverQueue.repeatMode = 0;
             message.channel.send('Looping is turned off.')
