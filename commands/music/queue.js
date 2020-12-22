c01dc4398767e52a14a6636034e93fa9a4c44b39
phene <60532517+joebobbio@users.noreply.github.com>
2020-12-22 17:18:11 -0500

    My code is the best code

--- commands/music/queue.js
+++ commands/music/queue.js
@@ -1,13 +1,13 @@
 const guild = require('discord.js');
 module.exports = {
 	name: 'queue',
-	description: 'get your server\'s music queue',
+	description: 'Show the queue',
 	cooldown: 2,
 	aliases: ['q'],
 	cd: "I just showed you the queue!",
 	execute(message, args, d) {
 		const serverQueue = message.client.queue.get(message.guild.id);
-		if (!serverQueue) return message.channel.send("There isn't a song playin");
+		if (!serverQueue) return message.channel.send("There's nothing playing.");
 		let loop;
 		switch (serverQueue.repeatMode) {
 			case 1:
@@ -25,7 +25,7 @@ module.exports = {
 			.setTitle('Song Queue')
 			.setDescription(loop + '\n_')
 			.setTimestamp()
-			.setFooter('DJ Grape');
+			.setFooter('DJ Bape');
 		for (var key in q) { queue.addFields({ name: '\u200b' + `${parseInt(key) + 1}` + '. ' + q[key].title, value: '_' }) }
 		message.channel.send(queue);
 	}
