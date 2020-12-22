10e08405a43344413ddbc5499581d62a8c9bebae
phene <60532517+joebobbio@users.noreply.github.com>
2020-12-22 00:10:16 -0500

    My code is the best code

--- commands/economy/daily.js
+++ commands/economy/daily.js
@@ -1,17 +1,17 @@
 module.exports = {
     name: 'daily',
-    description: 'get ur daily amount of stars',
+    description: `Free money?`,
     cooldown: 86400,
-    cd: "It's called daily for a reason, smh",
+    cd: "This command can only be used once a day! :rage:",
     async execute(message, args, d) {
         let random = Math.floor(Math.random() * 25) + 25;
         const dailystarEmbed = new d.Discord.MessageEmbed()
             .setColor('#dd2de0')
             .setTitle(message.author.username + `'s daily reward`)
             .addField('Daily Reward','here is ' + ` ${random} ` + ' :star:s')
-            .setThumbnail('https://i.imgur.com/JXfpgdXh.jpg')
+            .setThumbnail('https://cdn.discordapp.com/emojis/690075870995808326.png')
             .setTimestamp()
-            .setFooter('Grape Bank Inc.');
+            .setFooter('Bape Bank Inc.');
         message.channel.send(dailystarEmbed);
         d.addMoni(message.author.id, random);
     }
