89f76d0f397471c6ee53fbb8d9b73374c87f0f0b
phene <60532517+joebobbio@users.noreply.github.com>
2020-12-22 17:10:37 -0500

    My code is the best code

--- commands/moderation/unban.js
+++ commands/moderation/unban.js
@@ -1,35 +1,34 @@
 const Discord = require('discord.js');
 module.exports = {
     name: 'unban',
-    description: 'unban ppl',
     cooldown: 1,
     cd: "Chill on the unbans",
     execute(message, args, d) {
         let unbanUser = args[0];
         let boolean = message.member.hasPermission("BAN_MEMBERS");
         let myBoolean = message.guild.me.hasPermission("BAN_MEMBERS");
-        if (message.mentions.members.first()) { return message.channel.send('Bruh you gotta give me an id to unban'); }
+        if (message.mentions.members.first()) { return message.channel.send('You must provide an ID!'); }
         if (boolean && myBoolean) {
             if (unbanUser) {
                 if (message.author.id === unbanUser) {
-                    return message.channel.send('I don\'t think ur banned');
+                    return message.channel.send(`You aren't banned`);
                 }
                 if (message.client.user.id === unbanUser) {
-                    return message.channel.send('Bruh im not banned')
+                    return message.channel.send(`I too am not banned`)
                 }
                 try {
                     message.guild.members.unban(unbanUser)
                     message.channel.send("User unbanned!");
                 } catch {
-                    message.channel.send("I don't got permissions (or high enough role) to unban ppl. How about ya give me it?")
+                    message.channel.send("I cannot **unban members** in this guild. To complete this task, I need the `BAN_MEMBERS` permission.")
                 }
             }
         } else if (!unbanUser) {
-            message.channel.send('who are we unbanning? (you gotta give me their id bro)');
+            message.channel.send('You must provide an ID to unban!');
         } else if (!boolean) {
-            message.reply("bruh you dont even have permission to unban people, stop trying smh ");
+            message.reply("Permission denied.");
         } else if (!myBoolean) {
-            message.channel.send("I don't have perms")
+            message.channel.send("I cannot **unban members** in this guild. To complete this task, I need the `BAN_MEMBERS` permission.")
         } else {
             message.channel.send("Cannot unban that user");
         }
