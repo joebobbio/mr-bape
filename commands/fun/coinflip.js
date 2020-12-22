6c5819357e89ea2256e5e961a706ffee03edadf6
phene <60532517+joebobbio@users.noreply.github.com>
2020-12-22 17:03:12 -0500

    My code is the best code

--- commands/fun/coinflip.js
+++ commands/fun/coinflip.js
@@ -3,14 +3,14 @@ module.exports = {
     description: 'flip a coin',
     aliases: ['coin', 'cf'],
     cooldown: 5,
-    cd: "1 coinflip is enough",
+    cd: "flippy",
     execute(message, args, d) {
         const coin = new d.Discord.MessageEmbed()
             .setColor('#dd2de0')
             .setTitle(message.author.username + `'s coinflip`)
             .addField('It landed on', `${Math.round(Math.random()) ? 'Heads!' : 'Tails!'}`)
             .setTimestamp()
-            .setFooter('Grape Coin Flipper Club');
+            .setFooter('Bape Coin Flipper Club');
         message.channel.send(coin);
     }
 };
