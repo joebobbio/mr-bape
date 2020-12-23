91c559ad20a8fa145f4889c68f4adaf5bca165e2
phene <60532517+joebobbio@users.noreply.github.com>
2020-12-22 19:34:59 -0500

    My code is the best code

--- commands/music/stop.js
+++ commands/music/stop.js
@@ -1,15 +1,14 @@
 module.exports = {
 	name: 'stop',
-	description: 'stop playing music',
 	cooldown: 2,
 	aliases: ['leave', 'disconnect', 'dc'],
 	cd: "Why do you stop me so much?",
 	execute(message, args, d) {
 		const { channel } = message.member.voice;
 		const myChannel = message.guild.me.voice.channel;
-		if (!channel) return message.channel.send("Go to a voice channel to stop the music!");
+		if (!channel) return message.channel.send("You must first join a voice channel!");
 		const serverQueue = message.client.queue.get(message.guild.id);
-		if (!serverQueue && !myChannel) return message.channel.send("There ain't any music!");
+		if (!serverQueue && !myChannel) return message.channel.send("Nothing is playing.");
 		message.client.queue.delete(message.guild.id);
 		myChannel.leave();
 	}
