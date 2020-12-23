40de45f65b3b6555112eba19d54afd463be21001
phene <60532517+joebobbio@users.noreply.github.com>
2020-12-22 19:35:26 -0500

    My code is the best code

--- commands/music/volume.js
+++ commands/music/volume.js
@@ -1,21 +1,20 @@
 module.exports = {
     name: 'volume',
-    description: 'set volume of the bot',
     aliases: ['vol'],
     cd: "Enough volume cranking!",
     execute(message, args, d) {
         let title, number;
         let argument = args.join(' ');
         const { channel } = message.member.voice;
-        if (!channel) return message.channel.send('Get in a voice channel if you wanna pump it up!')
+        if (!channel) return message.channel.send('You must first join a voice channel!')
         const queue = message.client.queue.get(message.guild.id);
-        if (!queue) return message.channel.send('There ain\'t any music!')
+        if (!queue) return message.channel.send('Nothing is playing!')
         if (!argument) { title = 'Current Volume'; number = queue.volume }
         else {
             let set = parseInt(argument)
-			if (isNaN(set)) return message.channel.send("Give me a number nerd");
-            else if (set > 100) return message.channel.send("Let's not earrape ppl ok?")
-			else if (set < 0) return message.channel.send("Imagine negative volume");
+			if (isNaN(set)) return message.channel.send("Numbers only");
+            else if (set > 100) return message.channel.send("too loud!")
+			else if (set < 0) return message.channel.send("what does negative volume even sound like");
             queue.volume = set;
             queue.connection.dispatcher.setVolumeLogarithmic(set / 100);
             title = 'Volume set to'
