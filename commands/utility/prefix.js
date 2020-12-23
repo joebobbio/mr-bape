48d0edb41ca252d657a459ed5e7f69c6a5277de1
phene <60532517+joebobbio@users.noreply.github.com>
2020-12-22 20:30:59 -0500

    Update prefix.js

--- commands/utility/prefix.js
+++ commands/utility/prefix.js
@@ -4,6 +4,7 @@ module.exports = {
     cooldown: 2,
     cd: "Don't set prefix too often",
     async execute(message, args, d) {
+        if (!message.member.hasPermission("MANAGE_GUILD")) { return message.channel.send('Permission denied.'); }
         let guilds = await d.guilds.get(message.guild.id);
         if (!guilds) { guilds = {}; }
         if (!args[0]) { return message.channel.send(`Can't set the prefix if you don't tell me what to set it to.`) }
