7d22144a34d1d4fb3602ddcdb80b24bb8cce83ea
phene <60532517+joebobbio@users.noreply.github.com>
2020-12-22 19:39:04 -0500

    My code is the best code

--- commands/utility/prefix.js
+++ commands/utility/prefix.js
@@ -4,10 +4,10 @@ module.exports = {
     cooldown: 2,
     cd: "Don't set prefix too often",
     async execute(message, args, d) {
-        if (!message.member.hasPermission("MANAGE_GUILD")) { return message.channel.send('You don\'t have perms, git good.'); }
+        if (!message.member.hasPermission("MANAGE_GUILD")) { return message.channel.send('Permission denied.'); }
         let guilds = await d.guilds.get(message.guild.id);
         if (!guilds) { guilds = {}; }
-        if (!args[0]) { return message.channel.send('Whaddya want me to set the prefix to?') }
+        if (!args[0]) { return message.channel.send(`Can't set the prefix if you don't tell me what to set it to.`) }
         guilds.prefix = args[0];
         await d.guilds.set(message.guild.id, guilds);
         const prefixSetEmbed = new d.Discord.MessageEmbed()
@@ -17,7 +17,7 @@ module.exports = {
                 { name: 'Prefix', value: `${args[0]}` }
             )
             .setTimestamp()
-            .setFooter('Grape Databases');
+            .setFooter('Bape Databases');
         message.channel.send(prefixSetEmbed)
     }
 }; 
