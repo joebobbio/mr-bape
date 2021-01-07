5bdf6f7c811e85b56f2f090f9af0bcb27be36bc2
phene <60532517+joebobbio@users.noreply.github.com>
2021-01-07 10:43:18 -0500

    change invite

--- commands/utility/invite.js
+++ commands/utility/invite.js
@@ -4,7 +4,7 @@ module.exports = {
 	cooldown: 2,
 	cd: "I just gave you the link!",
 	execute(message, args, d) {
-		const invite = 'https://discord.com/api/oauth2/authorize?client_id=789949530131922975&permissions=1312156870&scope=bot';
+		const invite = 'https://discord.com/oauth2/authorize?client_id=796765069907329055&permissions=1312156870&scope=bot';
 		const inviteEmbed = new d.Discord.MessageEmbed()
 			.setColor('#dd2de0')
 			.setTitle('Invite')
